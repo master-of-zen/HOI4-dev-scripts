304256757042ac159910d8d796d258e892d68861
Grenight <true.grenigth@gmail.com>
2019-02-07 02:00:23 +0200

    Added comments

--- Settings.py
+++ Settings.py
@@ -1,3 +1,6 @@
+"""
+File for all literals
+"""
 
 idea_path = "/home/max/.local/share/Paradox Interactive/Hearts of Iron IV/mod/Blackice-hoi4/common/ideas"
 event_path = "/home/max/.local/share/Paradox Interactive/Hearts of Iron IV/mod/Blackice-hoi4/events/"
