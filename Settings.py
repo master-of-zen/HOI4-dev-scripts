0a134fca589e41e95ce108b2b6f21275b34c369f
Grenight <true.grenight@gmail.com>
2019-02-07 10:47:11 +0200

    +

--- Settings.py
+++ Settings.py
@@ -2,6 +2,13 @@
 File for all literals
 """
 
+"""Paths"""
+
 idea_path = "/home/max/.local/share/Paradox Interactive/Hearts of Iron IV/mod/Blackice-hoi4/common/ideas"
 event_path = "/home/max/.local/share/Paradox Interactive/Hearts of Iron IV/mod/Blackice-hoi4/events/"
 loc_path = "/home/max/.local/share/Paradox Interactive/Hearts of Iron IV/mod/Blackice-hoi4/localisation/"
+
+"""Strings"""
+
+strings = ("name", "desc", "title")
+forbidden = ("{", "}", "/", '"')
